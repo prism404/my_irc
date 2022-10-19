17c9567a65b010f0ed190c3a571203a859e8a772
prism <veronique.vdang@gmail.com>
2022-10-19 12:18:22 +0200

    connected to socket.io and define messages when the user is connected or disconnected

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const cors = require('cors');
-const mongoose = require('mongoose')
+const path = require('path');
+const http = require('http');
+const { Server } = require('socket.io');
+const mongoose = require('mongoose');
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
 require("dotenv").config();
 
 mongoose.connect(process.env.MONGO_DB, { 
@@ -15,8 +25,23 @@ mongoose.connect(process.env.MONGO_DB, {
 })
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+io.on('connection', (socket) => {
+    console.log(`${socket.id} is connected`);
+//     socket.emit('message', 'Welcome to Slide Chat !');
 
-const server = app.listen(process.env.PORT, () => {
+//     // Broadcast when an user enter a room
+//     socket.broadcast.emit('message', 'A user has joined the chat');
+
+//     // Runs when an user leave the room
+    socket.on('disconnect', () => {
+        console.log('A user has left the chat : ', socket.id);
+    });
+});
+
+
+
+server.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
+import io from "socket.io-client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Chat from "./pages/Chat";
+
+const socket = io.connect("http://localhost:2000");
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Chat />} />
       </Routes>
